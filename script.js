1acb3f0cd215b4e073bdadf03698c6932520d6f8
Brnjak6 <68736438+Brnjak6@users.noreply.github.com>
2021-04-08 22:56:40 +0200

    progress

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let priceTotal = document.getElementById('price-total')
 const sumButton = document.querySelector('.button-sum-btn')
 const selectAll = document.querySelectorAll('.selection__box')
 const priceAll = document.querySelectorAll('.price')
+const cpu = document.getElementById('cpu')
+const motherboard = document.getElementById('mobo')
+const gpu = document.getElementById('gpu')
 // First approach...
 // cpuSelect.addEventListener('click', function() {
 //    cpuPrice.innerHTML = cpuSelect.selectedOptions[0].dataset.price;
@@ -14,22 +17,46 @@ const priceAll = document.querySelectorAll('.price')
 //    ramPrice.innerText = ramSelect.selectedOptions[0].dataset.price
 // })
 
+
 selectAll.forEach(item => item.addEventListener('click', function(e) {
     const priceTarget = e.target.closest('.selection').querySelector('.price');
     priceTarget.textContent = this.selectedOptions[0].dataset.price;
 }))
 
+
 sumButton.addEventListener('click', function() {
     let total = 0;
 
     for (let i=0; i<priceAll.length; i++){
+        
         if(priceAll[i].innerHTML == 0) {
-            return priceTotal.innerHTML = 'Missing input'
-
+            return priceTotal.innerHTML = 'Input missing...';
         } else {
             total += parseInt(priceAll[i].innerHTML);
         }
       }
-      return priceTotal.innerHTML = total;
+      
+      priceTotal.innerHTML = total;
+
+      motherboard.value === cpu.value ? true : priceTotal.innerHTML = 'Can not combine am3 with am4 component';
+
+      motherboard.value === cooler.value ? true : priceTotal.innerHTML = 'Can not combine am3 with am4 component';
+
+    
+      
+      if(!priceTotal.innerHTML.includes('am3' || 'input')) {
+        document.querySelector('.message__content').classList.remove('hidden')
+      }
+
+      if(gpu.value === 'pot') {
+        priceTotal.textContent = 'Potato? Try again...'
+        setTimeout(() => {
+            document.querySelector('.challenge').textContent.add('🥔 * 20')
+        }, 2000);
+    }
+    
 })
+
+console.log('🥔' * 20);
+
 
